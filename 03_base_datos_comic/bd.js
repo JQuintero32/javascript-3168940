c86955eabccb01931c425e029f55423d7f634460
Juan José Quintero <jjquinterorios@gmail.com>
2025-11-05 17:23:08 -0500

    Add files via upload

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,32 @@
+let comic = {
+  "nombreComic": "La princesa ronca de oro",
+  "numeroEpisodeos": "3",
+  "genero": "Drama musical histórico",
+  "sinopsis": "Helena, una princesa nacida bajo una tormenta, desafía las normas de su reino para seguir su verdadera pasión: el canto. A través de su voz, enfrenta prejuicios, amores y traiciones, convirtiéndose en símbolo de libertad y empoderamiento femenino. Su legado trascenderá el tiempo como La Ronca de Oro, inspirando a otras mujeres a cantar sin miedo.",
+  "autores": ["Juan José Quintero", "Geraldine Ramirez", "Caleb Olaya"],
+  "portadaComic": "portada_principal.jpg",
+  "year": "",
+  "capitulos": [
+    {
+      "id": 1,
+      "nombre": "El surgimiento de la gran voz",
+      "personajes": "Helena (niña y adolescente), Doncellas, Partera, Conde Melódico",
+      "portada": "capitulo1.jpg",
+      "descripcion": "En una noche tormentosa nace Helena, la princesa que desde pequeña sueña con cantar. A pesar de las críticas, su voz empieza a brillar en los mercados y ferias, donde conoce al Conde Melódico, quien la invita a seguir su destino artístico."
+    },
+    {
+      "id": 2,
+      "nombre": "Romance, rebelión y traición",
+      "personajes": "Helena (adulta), Joven trovador, Público, Reyes, Damas, Narrador",
+      "portada": "capitulo2.jpg",
+      "descripcion": "Helena triunfa en el Teatro de las Mil Estrellas y se enamora de un trovador que traiciona su confianza. Tras su dolor, se reinventa con fuerza, levantando su voz como un himno de rebeldía ante una sociedad que intenta silenciarla."
+    },
+    {
+      "id": 3,
+      "nombre": "El legado del eco dorado",
+      "personajes": "Helena, Discípulas, Narrador",
+      "portada": "capitulo3.jpg",
+      "descripcion": "Refugiada en el Bosque de Ébano, Helena enseña su arte a otras mujeres y crea el Grimorio Musical de la Mujer Indomable. Su cuerpo desaparece, pero su voz se transforma en un eco eterno que inspira nuevas generaciones."
+    }
+  ]
+}
